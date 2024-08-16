48d4af6caa598368d01be360bb5f611e91f83a48
okemirio <okemirio@gmail.com>
2024-08-16 18:10:32 +0100

    next

--- index.js
+++ index.js
@@ -3,22 +3,20 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const products = require('./models/product');
 const Cart = require('./models/cart'); 
 const Order = require('./models/orders');
-const cors = require('cors')
 const UserModel = require('./models/user');
-const authRoutes =require('./routes/auth/index');
+const authRoutes = require('./routes/auth/index');
 const cartsRoutes = require('./routes/carts/carts');
 const productsRoutes = require('./routes/products/products');
 const OrdersRoutes = require('./routes/orders/orders');
 
-
-
 // Enable CORS for cross-origin requests
 const corsOptions = {
   origin: 'http://localhost:3000'
- }
+}
 
 // Load environment variables from .env file
 dotenv.config();
@@ -33,7 +31,8 @@ app.use(express.json());
 // MongoDB Atlas connection URI from environment variables
 const mongoURI = process.env.MONGO_URI;
 
-console.log(mongoURI)
+console.log('MongoDB URI:', mongoURI);
+
 // Connect to MongoDB
 mongoose.connect(mongoURI)
   .then(() => {
@@ -41,57 +40,19 @@ mongoose.connect(mongoURI)
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err.message);
-    
   });
-
-
 
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.use("/auth", authRoutes)
-app.use("/carts", cartsRoutes)
-app.use("/products", productsRoutes)
-app.use("/orders", OrdersRoutes)
+app.use("/auth", authRoutes);
+app.use("/carts", cartsRoutes);
+app.use("/products", productsRoutes);
+app.use("/orders", OrdersRoutes);
 
 
-// Authentication middleware
-const authenticateToken = (req, res, next) => {
-  // Get the token from the Authorization header
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-
-  if (!token) {
-    return res.sendStatus(401); // Unauthorized
-  }
-
-  // Verify the token
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return res.sendStatus(403); // Forbidden
-    }
-    req.user = user;
-    next();
-  
-  });
-};
-
-// Protected route
-app.get('/dashboard', authenticateToken, (req, res) => {
-  res.send('This is the dashboard');
-});
-
-
-
-
-
-
- 
-
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
