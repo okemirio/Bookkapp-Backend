e20fefc3d8509c2b09c20f941ce31baac81fcea2
okemirio <okemirio@gmail.com>
2024-08-19 19:55:59 +0100

    koi

--- index.js
+++ index.js
@@ -3,36 +3,38 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const UserModel = require("./models/user");
 const products = require("./models/product");
 const Cart = require("./models/cart");
 const Order = require("./models/orders");
-const cors = require("cors");
-const UserModel = require("./models/user");
 const authRoutes = require("./routes/auth/index");
 const cartsRoutes = require("./routes/carts/carts");
 const productsRoutes = require("./routes/products/products");
 const OrdersRoutes = require("./routes/orders/orders");
 
-// Enable CORS for cross-origin requests
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
-
 // Load environment variables from .env file
 dotenv.config();
 
 const app = express();
-const port = 'https://bookstore-alpha-silk.vercel.app';
+const port = process.env.PORT || 3000; // Use the environment variable for the port
+
+// Enable CORS for cross-origin requests
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://bookstore-alpha-silk.vercel.app"], // Allow both local and deployed frontend
+  credentials: true,
+};
+
 app.use(cors(corsOptions));
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
 // MongoDB Atlas connection URI from environment variables
-const mongoURI = process.env.MONGO_URI || "your_fallback_mongo_uri";
-
+const mongoURI = process.env.MONGO_URI;
 
 console.log(mongoURI);
+
 // Connect to MongoDB
 mongoose
   .connect(mongoURI)
@@ -51,18 +53,16 @@ app.use("/orders", OrdersRoutes);
 app.get('/dashboard', (req, res) => {
   res.send('This is the dashboard');
 });
+
 app.get('/', (req, res) => {
   res.send('This is the LOGIN');
-
 });
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// Export the app for Vercel
+module.exports = app;
